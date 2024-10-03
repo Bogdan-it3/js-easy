d353716c7838129b05c96048b5c1129033b2e7d2
Bogdan-it3 <kn23-b.barylovskyi@nubip.edu.ua>
2024-10-03 14:17:49 +0300

    Complete tasks

--- tasks/task2.js
+++ tasks/task2.js
@@ -2,8 +2,17 @@
 //  при цьому пропускаючи всі цифри.
 
 function reverseWithoutNumbers(str) {
-  // Ваш код тут
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+
+      if (isNaN(str[i]) && str[i] !== ' ') {
+          result += str[i];
+      }
+  }
+  return result; 
 }
+
+
 
 console.log(reverseWithoutNumbers("hello123world456")); // Виведе: "dlrowolleh"
 console.log(reverseWithoutNumbers("abc123xyz"));       // Виведе: "zyxabc"
